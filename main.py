81ee5b5f646be253edeb7553403c63ad5a2821cd
Noah Santos <n5santos@student.bridgew.edu>
2026-09-10 11:43:49 -0400

    more print + input

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 your_name = input("What is your name? ")
 print ("hello, " + your_name + "!")
+your_major = input("What is your major? ")
+print ("Wow, " + your_name + "!" + " Comp151 will definitely help you with " + your_major + "!")
